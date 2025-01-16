2ccdde25e54572a68693ec883e65a4476119568c
Herika <herikarajput3@gmail.com>
2025-01-16 10:25:30 +0530

    initial commit

--- JAVASCRIPT/closure.js
+++ JAVASCRIPT/closure.js
@@ -1,9 +1,9 @@
-// function outerfunction(name) {
-//     return function innerfunction() {
-//         console.log(`My name is ${name}`);
-//     }
-// }
-// const data = outerfunction("Herika")
-// data()
+function outerfunction(name) {
+    return function innerfunction() {
+        console.log(`My name is ${name}`);
+    }
+}
+const data = outerfunction("Herika")
+data()
 
 function outerfunction()
