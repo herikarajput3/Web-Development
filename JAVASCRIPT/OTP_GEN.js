0694a1a811b3d6d42afa07b470f5e7d74441baa5
Herika <herikarajput3@gmail.com>
2025-01-29 22:38:15 +0530

    OTP Generation using math.random()

--- JAVASCRIPT/OTP_GEN.js
+++ JAVASCRIPT/OTP_GEN.js
@@ -1,12 +1,9 @@
 function OTP(num) {
     // console.log(Math.floor(Math.random() * 1000000) + 1);
-    let otp;
+    let otp = "";
     for (let i = 0; i < num; i++) {
-        otp = Math.floor(Math.random() * num) + 1;
+        otp += Math.floor(Math.random() * 10).toString();
     }
     return otp;
 }
 console.log(OTP(6));
-
-
-// OTP(6)
