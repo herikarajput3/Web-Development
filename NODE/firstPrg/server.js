e525cfdc9635d8c1cfc2990ee8933b28a892a42c
Herika <herikarajput3@gmail.com>
2025-06-09 16:52:21 +0530

    Initial commit

--- NODE/firstPrg/server.js
+++ NODE/firstPrg/server.js
@@ -1,1 +1,12 @@
-console.log("Herika Rajput");
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.end("hello world");
+})
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
