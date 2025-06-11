76cf84b85206c653b0146d8d04cf296bdb7895fc
Herika <herikarajput3@gmail.com>
2025-06-11 09:25:55 +0530

    Initial commit

--- NODE/firstPrg/express.js
+++ NODE/firstPrg/express.js
@@ -3,13 +3,13 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
     .connect('mongodb://localhost:27017/firstDb')
-    .then(() => console.log("db connected successfully 👍"))
-    .catch((err) => console.log(err, "db connection failed 👎"));
+    .then(() => console.log("Database connected successfully"))
+    .catch((err) => console.log(err, "Database connection failed"));
 
 const userSchema = new mongoose.Schema({
     name: String,
@@ -17,22 +17,22 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const userModal = mongoose.model("userModel", userSchema);
+const userModel = mongoose.model('userModel', userSchema);
 
 app.post('/register', async (req, res) => {
     try {
-        const { name, email, password } = req.body;
-        console.log(name, email, password);
-        const userData = await userModal.create({ name, email, password });
-        if (!userData) {
-            res.status(400).json({ message: "user not created" });
+        const user = await userModel.create(req.body);
+
+        if (!user) {
+            res.status(400).json({ message: "User not created" });
         } else {
-            res.status(201).json({ message: "user created successfully", userData });
+            res.status(201).json({ message: "User created successfully", user });
         }
     } catch (error) {
-        res.status(400).json({ message: "something went wrong" });
+        res.status(500).json({ message: error.message });
     }
 })
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
