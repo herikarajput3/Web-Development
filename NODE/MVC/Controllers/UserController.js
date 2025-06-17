e4502d56829c10640fc70b09c08d3f7200fdeb98
Herika <herikarajput3@gmail.com>
2025-06-17 11:36:32 +0530

    Crud using MVC

--- NODE/MVC/Controllers/UserController.js
+++ NODE/MVC/Controllers/UserController.js
@@ -39,18 +39,67 @@ exports.userLogin = async (req, res) => {
     }
 }
 
-// exports.userFind = async (req, res) => {
-//     try {
-//         const users = await User.find();
+exports.userFind = async (req, res) => {
+    try {
+        const users = await User.find();
 
-//         if (users) {
-//             res.status(200).json({ message: "Users fetched Successfully", users });
-//         } else {
-//             res.status(400).json({ message: "Users not fetched" });
-//         }
-//     } catch (error) {
-//         console.log("Error: ", error);
-//         res.status(500).json({ message: error.errmsg });
+        if (users) {
+            res.status(200).json({ message: "Users fetched Successfully", users });
+        } else {
+            res.status(400).json({ message: "Users not fetched" });
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json({ message: error.errmsg });
 
-//     }
-// };
+    }
+};
+
+exports.getParticularUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userData = await User.findById(id);
+        if (userData) {
+            res.status(200).json({ message: "User fetched Successfully", userData });
+        } else {
+            res.status(400).json({ message: "User not found" });
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json({ message: "Internal server error" }, error.errmsg);
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userData = await User.findByIdAndDelete(id);
+        if (userData) {
+            res.status(200).json({ message: "User Deleted Successfully", userData });
+        } else {
+            res.status(400).json({ message: "User not deleted" });
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json({ message: "Internal server error" }, error.errmsg);
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email } = req.body;
+        const userData = await User.findByIdAndUpdate(id, {
+            name,
+            email
+        })
+        if (userData) {
+            res.status(200).json({ message: "User Updated Successfully", userData });
+        } else {
+            res.status(400).json({ message: "User data not updated" });
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json({ message: "Internal server error" }, error.errmsg);
+    }
+}
--- NODE/MVC/Routes/routes.js
+++ NODE/MVC/Routes/routes.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const { userRegister, userFind, userLogin } = require('../Controllers/UserController');
+const { userRegister, userFind, userLogin, getParticularUser, deleteUser, updateUser } = require('../Controllers/UserController');
 const router = express.Router()
 require('../db')
 
 router.post('/register', userRegister);
 router.post('/login', userLogin);
-// router.get('/users', userFind);
+router.get('/users', userFind);
+router.get('/getParticularUser/:id',getParticularUser);
+router.delete('/deleteUser/:id',deleteUser);
+router.put('/updateUser/:id',updateUser);
 
 module.exports = router;
