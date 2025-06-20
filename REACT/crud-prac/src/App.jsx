e3dcc2b1abe8af719396d8f3660ac9917cefe9bf
Herika <herikarajput3@gmail.com>
2025-06-20 14:24:08 +0530

    Initial commit

--- REACT/crud-prac/src/App.jsx
+++ REACT/crud-prac/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import AI_Form from './View/Pages/AI_Form'
 import First_form_prac from './View/Pages/First_form_prac'
 import Form from './View/Pages/Form'
 import Second_Form_prac from './View/Pages/Second_Form_prac'
@@ -10,7 +9,6 @@ function App() {
     <>
       {/* <First_form_prac /> */}
       {/* <Second_Form_prac/> */}
-      {/* <AI_Form /> */}
       <Form />
     </>
   )
