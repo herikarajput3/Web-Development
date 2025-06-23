ffb9db59c841f9854e69a933aad9df81c6108c24
Herika <herikarajput3@gmail.com>
2025-06-23 12:25:30 +0530

    CRUD operation using react hook form

--- REACT/crud-prac/src/View/Pages/Third_form_prac.jsx
+++ REACT/crud-prac/src/View/Pages/Third_form_prac.jsx
@@ -1,20 +1,51 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 const Third_form_prac = () => {
-    const [data, setData] = useState({
-        fname: '',
-        lname: '',
-        gender: '',
-    })
-
+    const [users, setUsers] = useState([])
+    const [editUser, setEditUser] = useState(null)
     const { register, handleSubmit, reset } = useForm();
 
     const onSubmit = async (data) => {
-        await axios.post('http://localhost:3000/users', data);
-        reset();
+        if (editUser) {
+            const res = await axios.patch(`http://localhost:3000/users/${editUser}`, data);
+            setEditUser(null);
+            fetchUsers();
+        }
+        else {
+            await axios.post('http://localhost:3000/users', data);
+            fetchUsers();
+        }
+        reset({
+            fname: '',
+            lname: '',
+            gender: ''
+        });
     }
+
+    const fetchUsers = async () => {
+        const res = await axios.get('http://localhost:3000/users');
+        setUsers(res.data);
+    }
+
+    const handleEdit = (data) => {
+        reset({
+            fname: data.fname,
+            lname: data.lname,
+            gender: data.gender
+        })
+        setEditUser(data.id);
+    }
+
+    const handleDelete = async (id) => {
+        await axios.delete(`http://localhost:3000/users/${id}`);
+        fetchUsers();
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
     return (
         <>
             <div className="container">
@@ -31,8 +62,8 @@ const Third_form_prac = () => {
                                 <input type="text" className='form-control' id='lname' {...register("lname")} />
                             </div>
                             <div className="form-group mb-3">
-                                <label htmlFor="gender" className='ms-2'>Gender</label>
-                                <select name="gender" id="gender" className='form-select ms-2' {...register("gender")}>
+                                <label htmlFor="gender" className=''>Gender</label>
+                                <select name="gender" id="gender" className='form-select' {...register("gender")}>
                                     <option value="" disabled >Select your gender</option>
                                     <option value="female">Female</option>
                                     <option value="male">Male</option>
@@ -40,6 +71,39 @@ const Third_form_prac = () => {
                             </div>
                             <button type='submit' className='btn btn-primary'>Submit</button>
                         </form>
+                    </div>
+                    <div className="col-12">
+                        <table className='table table-striped table-hover mt-5 rounded'>
+                            <thead className='table-dark'>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th>Gender</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.length > 0 ? (
+                                    users.map((user, index) => (
+                                        <tr key={user.id}>
+                                            <td>{index + 1}</td>
+                                            <td>{user.fname} {user.lname}</td>
+                                            <td>{user.gender}</td>
+                                            <td>
+                                                <div>
+                                                    <button className='btn btn-primary me-3' onClick={() => handleEdit(user)}>Edit</button>
+                                                    <button className='btn btn-danger' onClick={() => handleDelete(user.id)}>Delete</button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))
+                                ) : (
+                                    <tr>
+                                        <td colSpan={4} className='text-center fw-bold fs-5'>No Data Found</td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
